c594fad530bc416de272ffda7b187edd13eebbbe
lillianlaihughes <lillianlaihughes@gmail.com>
2023-04-04 15:13:40 -0400

    updated js for all shapes

--- index.js
+++ index.js
@@ -1,102 +1,43 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown')
+const maxLength = require('inquirer-maxlength-input-prompt')
+
+inquirer.registerPrompt('maxlength-input', maxLength)
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your Github username.',
+        type: 'maxlength-input',
+        name: 'characters',
+        message: 'Enter up to 3 characters',
+        maxLength: 3
     },
     {
         type: 'input',
-        name: 'email',
-        message: 'Enter your email address.',
-    },
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Enter your Project Title.',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Write a short description of your Project.',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'What are the installation instructions? (What command should be run to install dependencies?)',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'What is the usage information? (What does the user need to know about using the repo?)',
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'What are the contribution guidelines? (What does the user need to know about contributing to the repo?)',
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'What are the test instructions? (What command should be run to run tests?)',
+        name: 'color-chara',
+        message: 'Enter a text color (keyword or hex #).',
     },
     {
         type: 'list',
-        name: 'license',
-        message: 'Select a license for your project.',
-        choices: ['Apache_2.0', 'MIT', 'GPLv3', 'BSD_3', 'None']
+        name: 'shape',
+        message: 'Choose a shape.',
+        choices: ['circle', 'triangle', 'square']
+    },
+    {
+        type: 'input',
+        name: 'color-shape',
+        message: 'Enter a shape color (ideally a different color than the one you entered above!).',
     }
 ];
-
-// TODO: Create a function to write README file
-function writeToFile(filename, data) {
-    return fs.writeFileSync('./readme_output/README.md', data);
-    // NOTE TO SELF: Review fs.writeFileSync doesn't require a callback param like fs.writeFile does
-    // NOTE TO SELF: Should I create a new folder to put READMEs created from the CLI?
-}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
     .prompt(questions)
     .then((answers) => {
-        console.log('Generating README file...')
-        writeToFile('README.md', generateMarkdown({...answers})); // REVIEW ... rest/spread        
+        console.log('Generated logo.svg') 
     })
 }
 // Function call to initialize app
 init();
-
-// REVIEW FROM CLASS ACTIVITIES
-// inquirer
-//   .prompt([
-//     {
-//       type: 'input',
-//       name: 'name',
-//       message: 'What is your name?',
-//     },
-//     {
-//       type: 'checkbox',
-//       message: 'What languages do you know?',
-//       name: 'stack',
-//       choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
-//     },
-//     {
-//       type: 'list',
-//       message: 'What is your preferred method of communication?',
-//       name: 'contact',
-//       choices: ['email', 'phone', 'telekinesis'],
-//     },
-//   ])
-//   .then((data) => {
-//     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-
-//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-//       err ? console.log(err) : console.log('Success!')
-//     );
-//   });
